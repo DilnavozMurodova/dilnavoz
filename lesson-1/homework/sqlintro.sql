197edb76a2407a26bdab4432eda86f66effd9613
DilnavozMurodova <dilnoza4467400@gmail.com>
2025-05-25 13:26:48 +0500

    Update sqlintro.sql

--- lesson-1/homework/sqlintro.sql
+++ lesson-1/homework/sqlintro.sql
@@ -1,1 +1,20 @@
-hello 
+1---- Homework Answers 
+1.Data- Factual information such as measurement or statistics used as a basis for reasoning,discussing,calculation.Data is gathered information fot scientific purposes				
+Database --collection of hihgly structured tables,wherein each row reflectsa data entity  and every column defines a specific information.
+
+Relational database --Includes tables containing rows and columns.
+Table--databese object that contain all the data i  a database.In tables, data is logically organized in a row and column format.
+
+2.5 Key features of SQL:Easy Management,Open Source, Highy Security, Scalability and Flexibility.
+3.wo-Factor Authentication (2FA)
+Single Sign-On (SSO)
+4.Create database schoolIDB;
+5.create table Students1 (studentId int, name varchar (50), Age int);
+6.SQL--the language of query
+SQL Server--database management system
+SSMS---SQL Server Management Studio ,is a powerful software application developed by Microsoft for managing SQl databases
+7.DQL-Data Query Language  
+DML--Data Manipulation Language,used to change database (Insert,Update,Delete)
+DDL--Data definion Language,provides to change table structure(create,alter,drop)
+DCL--Data control Language, uses grant and revoke
+TCL-Transaction Control language (Commit,Rollback,Savepoint)
